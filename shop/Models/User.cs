cb3af111441452d8d5bb47d19ba227f87bc56e13
user <user@DESKTOP-LVQ4TD5>
2023-03-21 06:59:57 +0300

    a

--- shop/Models/User.cs
+++ shop/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace shop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace shop.Models
 {
     public partial class User
     {
